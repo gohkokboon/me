0c5f240a3861eed3e8d86b36fecd3b2e1c282de6
gohkokboon <guowen4567@gmail.com>
2019-12-22 23:42:54 +0800

    Updates

---
+++ precache-manifest.c3fd301be771ecacaa4a1013bde0dc04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.66be97a1.js"
+  },
+  {
+    "revision": "ffbc82d6d79e84df8bc1",
+    "url": "/me/static/js/main.ffbc82d6.chunk.js"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/me/static/js/2.9c5ca2f4.chunk.js"
+  },
+  {
+    "revision": "ffbc82d6d79e84df8bc1",
+    "url": "/me/static/css/main.5fc6f3d3.chunk.css"
+  },
+  {
+    "revision": "6e03ac4037a0b80c163cdb85535ccbdb",
+    "url": "/me/index.html"
+  }
+];
